b949b583e35001983db9bcdba7f0a10f66ecd32b
Ahmet0z <l9120016@std.yildiz.edu.tr>
2021-09-28 17:37:49 +0300

    17. Ders Ödevler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CarDetailComponent } from './components/car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    NaviComponent,
+    ColorComponent,
+    CustomerComponent,
+    RentalComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car{
+    id:number;
+    brandName:string;
+    colorName:string;
+    carName:string;
+    modelYear:number;
+    description:string;
+    dailyPrice:number;
+}
